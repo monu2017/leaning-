d9365bacf160f9b02b06f7d16c273579427d2e2d
monu <monusinghk@gmail.com>
2020-04-30 12:53:15 +0530

    newfile

---
+++ app.py
@@ -0,0 +1,8 @@
+course = 'monu singh'
+print('Monu' in course)
+print(course.find('i'))
+print(course.upper())
+print(len(course))
+print(course.replace('singh', 'kumar'))
+print(course)
+print('monu' in course)
